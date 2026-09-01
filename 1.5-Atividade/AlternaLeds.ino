08ae21a5f46aa4350fdeee83cee5434f0b4ca72c
gui <guilhermeangelozzi@gmail.com>
2026-09-01 01:51:54 -0300

    Update AlternaLeds.ino

--- 1.5-Atividade/AlternaLeds.ino
+++ 1.5-Atividade/AlternaLeds.ino
@@ -1,1 +1,26 @@
+/*
+Faça um programa que:
+> inicie com todos os leds apagados
+> ao Clicar uma vez no "Botão Azul" acenda o Led Azul, mantenha aceso e apague o Verde
+> ao Clicar uma vez no "Botão Verde" acenda o Led Verde, mantenha aceso e apague o Azul
+*/
 
+void loop()
+{
+  
+  // Se clicar no botão Verde (pino 2), acende Verde (13) e apaga Azul (11)
+  if (digitalRead(2) == HIGH) 
+  {
+    digitalWrite(13, HIGH);
+    digitalWrite(11, LOW);
+  } 
+  
+  // Se clicar no botão Azul (pino 7), acende Azul (11) e apaga Verde (13)
+  if (digitalRead(7) == HIGH) 
+  {
+    digitalWrite(11, HIGH);
+    digitalWrite(13, LOW);
+  }
+  
+  delay(10);
+}
